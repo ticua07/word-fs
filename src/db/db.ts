fd54e4d54a1aeedcbfd19fbe3ef37d9b5196c6e5
ticua07 <ticua@ticua.ar>
2024-03-17 15:55:35 -0300

    finish db schema

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,5 +1,6 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 
+console.log({ url: process.env.TURSO_DB_URL!, authToken: process.env.TURSO_DB_TOKEN! })
 const client = createClient({ url: process.env.TURSO_DB_URL!, authToken: process.env.TURSO_DB_TOKEN! });
 export const db = drizzle(client);
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,5 +4,7 @@ import { text, sqliteTable, blob } from "drizzle-orm/sqlite-core";
 export const files = sqliteTable('files', {
     words: text("words"),
     data: blob("data"),
+    filename: text("filename"),
+    mimeType: text("mimetype"),
     createdAt: text("createdAt").default(sql`CURRENT_TIMESTAMP`),
 });
