606e9bb4c5700a880a7c35f8370cf1dc5a970ba6
ticua07 <ticua@ticua.ar>
2024-03-20 20:22:37 -0300

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
-  output: "server"
+  output: "server",
+  adapter: vercel()
 });
